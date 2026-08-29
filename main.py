f3bc2e2a510c506a897db1b9e66257ee3da80453
Abhijith <987abhijith654@gmail.com>
2026-08-29 14:08:18 +0530

    Cleaning DOM Content

--- main.py
+++ main.py
@@ -3,7 +3,13 @@
 
 import streamlit as st
 # Imported streamlit
-from scrape import scrape_website   #Importing the scrape_website function from scrape 
+from scrape import (
+    scrape_website,
+    split_dom_content,
+    clean_body_content,
+    extract_body_content
+)
+#Importing functions from scrape 
 
 st.title("AI Web Scrapper")
 #Used title as AI Web Scrapper
@@ -15,6 +21,18 @@ url = st.text_input("Enter a Website URL")
 if st.button("Scrape the Site"):    # Creates a button namely Scrape the site
     st.write("Scraping the website")    # After clicking we get this message
 
-    result = scrape_website(url)    #passing the url to the function 
-    print(result)   # the result is the html source code 
+    result = scrape_website(url)    #passing the url to the function contains html source also
+
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+
+    # Stores the cleaned_content  in streamlit's session state
+    # so it can be accessed again even if streamlit reruns the script 
+
+    st.session_state.dom_content = cleaned_content 
+
+    # Creates a collapsable section to view the scrapped DOM content 
+    with st.expander("View DOM Cotent"):
+        
+        st.text_area("DOM Content",cleaned_content,height=300)  # Contains DOM contents with height of 300
 
--- scrape.py
+++ scrape.py
@@ -9,6 +9,8 @@ import selenium.webdriver as webdriver  # webdriver is like the controller to co
 from selenium.webdriver.chrome.service import Service   ## Import Service class to tell Selenium where the ChromeDriver executable is
 from config import SBR_WEBDRIVER    # A file config containing the API Key 
 
+from bs4 import BeautifulSoup
+
 # Define a function that takes a website URL and returns its HTML content
 def scrape_website(website):
     print("Launching chrome browser...")
@@ -36,3 +38,38 @@ def scrape_website(website):
     finally:
         # Close the browser window and end the session
         driver.quit()
+
+# We only need the body from the html so we use this function
+def extract_body_content(htmlcontent):
+    soup = BeautifulSoup(htmlcontent,"html.parser") # BeautifulSoup is a Python library used to read and work with HTML
+
+    body_content = soup.body # This provides the body
+
+    if body_content:    #If body exists
+        return str(body_content)
+    
+    return ""   #else empty string
+
+# We must remove the scripts and style tags in the body content, so we are using this functio
+def clean_body_content(body_content):
+
+    soup = BeautifulSoup(body_content,"html.parser")
+
+    for script_or_style  in soup(["script","style"]):   # Fetch the script and style
+        script_or_style.extract()   # Extract/Remove them
+
+    cleaned_content = soup.get_text(separator="\n")     # Add new line as separator for the text
+
+    # If new line is extra we remove it with this
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip() 
+    )
+
+    return cleaned_content
+
+# LLM's can read upto 8000 characters of data as they have limit so we just give the data in chunks
+# allowing it to read everything
+def split_dom_content(dom_content,maxlength = 6000):
+    return [
+        dom_content[i:i+maxlength] for i in range(0,len(dom_content),maxlength)
+    ]
