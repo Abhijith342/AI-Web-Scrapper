28c30b8b5d0ce35d8c01ddc4fab8884f34dac8b5
Abhijith <987abhijith654@gmail.com>
2026-08-29 12:09:01 +0530

    Used brightdata as captcha solver and changed from Chrome to Remote browser

--- scrape.py
+++ scrape.py
@@ -7,31 +7,29 @@
 
 import selenium.webdriver as webdriver  # webdriver is like the controller to control the site 
 from selenium.webdriver.chrome.service import Service   ## Import Service class to tell Selenium where the ChromeDriver executable is
-import time ## Import time module to add delays (sleep)
-
+from config import SBR_WEBDRIVER    # A file config containing the API Key 
 
 # Define a function that takes a website URL and returns its HTML content
 def scrape_website(website):
     print("Launching chrome browser...")
 
-    # Path to your ChromeDriver executable (must match your Chrome version)
-    chrome_driver_path = "./chromedriver.exe"
-
     options = webdriver.ChromeOptions() # Create Chrome options (you can add settings here like headless mode)
 
     # Start a new Chrome browser using the driver and options
-    driver = webdriver.Chrome(service=Service(chrome_driver_path),options=options)
+    # driver = webdriver.Chrome(service=Service(chrome_driver_path),options=options)
+
+    # driver is now Remote based on the API options
+    driver = webdriver.Remote(
+        command_executor=SBR_WEBDRIVER,
+        options=options
+    )
 
     try:
         # Open the given website in the browser
         driver.get(website)
         print("Page loaded...")
 
-
         html = driver.page_source
-
-        # Pause for 10 seconds (so you can see the browser before it closes)
-        time.sleep(10)
 
         return html
 
